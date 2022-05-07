82d2afb0b9dac28bac916c5d118e5c77fd512c98
Hong Shik Branden Kim <kimploo@gmail.com>
2022-05-08 01:17:05 +0900

    student repository

--- getListMultiplesOfTwo.js
+++ getListMultiplesOfTwo.js
@@ -1,7 +1,7 @@
-const { range } = require('range');
+const { range } = require('range'); // range 모듈을 불러옵니다
 
 function getListMultiplesOfTwo(upTo) {
-  return range(2, upTo + 1, 2)
+  // TODO
 }
 
 module.exports = getListMultiplesOfTwo;
