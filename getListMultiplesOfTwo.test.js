283fb987b6c5833cb2362a2e9519dc87bee08e2d
Hong Shik Branden Kim <kimploo@gmail.com>
2022-05-07 22:57:59 +0900

    student repository

--- getListMultiplesOfTwo.test.js
+++ getListMultiplesOfTwo.test.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 const getListMultiplesOfTwo = require('./getListMultiplesOfTwo')
+const { scripts } = require('./package.json')
 
 describe('', function () {
   it('배열을 리턴해야 합니다', function () {
@@ -24,4 +25,8 @@ describe('', function () {
       90, 92, 94, 96, 98, 100
     ]);
   });
+
+  it('npm run start 스크립트를 통해 index.js 파일을 실행해야 합니다', function () {
+    expect(scripts.start).to.be.eql('node index.js')
+  })
 })
